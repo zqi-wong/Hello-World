eed69e6f20cfd1fb3de0bbb1e175db795af89c68
Ziqian Wang <2779671262@qq.com>
2020-10-17 22:47:41 +0800

    make a programme which can help sign in and sign up

---
+++ sign_up_and_sign_in.py
@@ -0,0 +1,50 @@
+import json
+
+def get_usernames():
+    filename = "usernames.json"
+    try:
+        with open(filename) as obs:
+            usernames = json.load(obs)
+    except FileNotFoundError:
+        usernames = {}
+    return usernames
+
+def sign_up(usernames):
+    key = input("Please input your account:(If you want to quit, input 'q'.)")
+    value = input("Please input your password:")
+    for account in usernames.keys():
+        if account == key:
+            print("You have been signed up.Try to sign in.")
+            return
+    usernames[key] = value
+
+def sign_in(usernames,b=False):
+    key = input("Please input your account:(If you want to quit, input 'q'.)")
+    while key != 'q':
+        value = input("Please input your password:")
+        if value == usernames[key]:
+            print("You have been signed in.")
+            b = True
+            break
+        else:
+            print("You input a wrong password.Please try it again.\n")
+        key = input("Please input your account:(If you want to quit input 'q'.)")
+        value = input("Please input your password:")
+
+usernames = get_usernames()
+b = False
+while True:
+    order = input("What order do you want to make?Please input 'sign in' , 'sign up' or 'quit'\n")
+    if order == 'sign in':
+        sign_in(usernames,b)
+        break
+    elif order == 'sign up':
+        sign_up(usernames)
+        break
+    elif order == 'quit':
+        break
+    else:
+        print("You have inputed sth wrong.Please try again.")
+filename = "usernames.json"
+with open(filename,'w') as ob:
+    json.dump(usernames,ob)
