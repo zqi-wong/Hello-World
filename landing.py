c10ac6702174cf397d46e9a1c0bc240a497be359
Ziqian Wang <2779671262@qq.com>
2020-10-17 23:24:37 +0800

    do some betterings

--- landing.py
+++ landing.py
@@ -10,44 +10,49 @@ def get_usernames():
     return usernames
 
 def sign_up(usernames):
-    key = input("Please input your account:(If you want to quit, input 'q'.)")
+    key = input("\nSIGN UP:\nPlease input your account:(If you want to quit, input 'q'.)")
     if key == 'q':
         return
     value = input("Please input your password:")
     for account in usernames.keys():
         if account == key:
-            print("You had been signed up.Try to sign in then.\n")
+            print("You had been signed up.Try to sign in then.")
             return True
     usernames[key] = value
 
-def sign_in(usernames,b=False):
-    key = input("Please input your account:(If you want to quit, input 'q'.)")
+def sign_in(usernames):
+    global b
+    b = False
+    key = input("\nSIGN IN:\nPlease input your account:(If you want to quit, input 'q'.)")
     while key != 'q':
         value = input("Please input your password:")
-        if value == usernames[key]:
-            print("You have been signed in.")
-            b = True
-            break
-        else:
-            print("You input a wrong password.Please try it again.\n")
+        try:
+            if value == usernames[key]:
+                print("You have been signed in.")
+                b = True
+                break
+            else:
+                print("You input a wrong password.Please try it again.\n")
+        except KeyError:
+            print("We don't have this account.Please sign up first.")
+            return True
         key = input("Please input your account:(If you want to quit input 'q'.)")
-        value = input("Please input your password:")
 
 usernames = get_usernames()
-b = False
 while True:
-    order = input("What order do you want to make?Please input 'sign in' , 'sign up' or 'quit'\n")
+    order = input("\nWhat order do you want to make?Please input 'sign in' , 'sign up' or 'quit'\n")
     if order == 'sign in':
-        sign_in(usernames,b)
-        break
+        if sign_in(usernames):
+            sign_up(usernames)
     elif order == 'sign up':
         if sign_up(usernames):
-            sign_in(usernames,b)
-        break
+            sign_in(usernames)
     elif order == 'quit':
         break
     else:
-        print("You have inputed sth wrong.Please try again.")
+        print("You have inputed sth wrong.Please try again.\n")
+    if b:
+        break
 filename = "usernames.json"
 with open(filename,'w') as ob:
     json.dump(usernames,ob)
